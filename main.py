ec6c757b89ab680768da0308f60066a574201ffb
Ayush Pattnaik <pattnaikayush21@gmail.com>
2020-10-11 03:50:23 +0530

    welcomescreen

--- main.py
+++ main.py
@@ -18,6 +18,31 @@ PIPE='/Gallery/Sprites/pipe.png'
 
 def welcomeScreen():
     pass
+    playerx=int(SCREENWIDTH/5)
+    playery=int(SCREENHEIGHT-GAME_SPRITES['player'].get_height())/2 
+    messagex=int(SCREENWIDTH-GAME_SPRITES['message'].get_height())/2 
+    messagey=int(SCREENHEIGHT*0.8)
+    basex=0
+    basey=int(SCREENHEIGHT)
+    while True:
+        for event in pygame.event.get():
+            if event.type==QUIT or (event.type==KEYDOWN and event.key==K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            elif event.type==KEYDOWN and (event.key==K_SPACE or event.key==K_UP):
+                return 
+            else:
+                SCREEN.blit(GAME_SPRITES['background'],(0,0))
+                SCREEN.blit(GAME_SPRITES['player'],(playerx,playery))
+                SCREEN.blit(GAME_SPRITES['message'],(messagex,messagey))
+                SCREEN.blit(GAME_SPRITES['base'],(basex,GROUNDY)) 
+                pygame.display.update()
+                FPSCLOCK.tick(FPS)
+def mainGame():
+    score=0
+    playerx=int(SCREENWIDTH/5)
+    playery=int(SCREENWIDTH/2)
+
 
 if __name__ == '__main__':
     pygame.init()
@@ -48,7 +73,7 @@ if __name__ == '__main__':
     GAME_SOUNDS['swoosh']=pygame.mixer.Sound('');
     GAME_SOUNDS['wing']=pygame.mixer.Sound(''); 
     
-    GAME_SPRITES['message']=pygame.image.load(BACKGROUND).convert()
+    GAME_SPRITES['message'] =pygame.image.load('').convert_alpha()
     GAME_SPRITES['background']=pygame.image.load(BACKGROUND).convert()
     GAME_SPRITES['player']=pygame.image.load(PLAYER).convert_alpha()
 
